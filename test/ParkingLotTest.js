3192e0cf621dbad1f6a8b475410a0cf54f07b809
GaikwadRavi03 <gaikwadr576@gmail.com>
2020-01-02 17:36:30 +0530

    Added testCases for when parking lot is full.

--- test/ParkingLotTest.js
+++ test/ParkingLotTest.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var ParkingLotSystem = require('./ParkingLotSystem');
+var ParkingLotSystem = require('../main/ParkingLotSystem');
 describe('describe Mocha Test for parking lot', function () {
     it('should return true when park their car to catch the flight.', function () {
         let parkingLotSystem = new ParkingLotSystem();
@@ -7,14 +7,20 @@ describe('describe Mocha Test for parking lot', function () {
         let ans = parkingLotSystem.park(car);
         assert.equal(ans, true);
     });
-    it('should return true when Unpark their car to go home.', function () {
+    it('should return false when no vehicle park.', function () {
+        let parkingLotSystem = new ParkingLotSystem();
+        let car = new Object();
+        let ans = parkingLotSystem.park();
+        assert.equal(ans, false);
+    });
+    it('should return true when UnPark their car to go home.', function () {
         let parkingLotSystem = new ParkingLotSystem();
         let car = new Object();
         parkingLotSystem.park(car);
         let ans = parkingLotSystem.unPark(car);
         assert.equal(ans, true);
     });
-    it('should return throw an exception when Unpark unknown vehicle.', function () {
+    it('should return throw an exception when UnPark unknown vehicle.', function () {
         try {
             let parkingLotSystem = new ParkingLotSystem();
             let car = new Object();
@@ -23,7 +29,7 @@ describe('describe Mocha Test for parking lot', function () {
             parkingLotSystem.park(car1);
             let ans = parkingLotSystem.unPark(new Object());
         } catch (error) {
-            assert.equal(error.message, 'unknown vehicle unparked.');
+            assert.equal(error.message, 'unknown vehicle unParked.');
         }
     });
     it('should return exception when parking lot is full.', function () {
