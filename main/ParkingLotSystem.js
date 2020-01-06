a93d7a0fbbcc1206431288f5aca18ba448232c70
GaikwadRavi03 <gaikwadr576@gmail.com>
2020-01-06 14:56:37 +0530

    Refactored code to car park evenly distribution and Added code for handicap driver to park his car nearest space.

--- main/ParkingLotSystem.js
+++ main/ParkingLotSystem.js
@@ -1,30 +1,37 @@
 let ParkingLotObserver = require('./ParkingLotObserver');
 
 let parkingLotObserver
-const parkingLotMaxSize = 4
+const parkingLotMaxSize = 9
 
 class ParkingLotSystem {
 
     constructor() {
-        this.parkingLots = [];
+        this.parkingLots = 0;
+        this.counter = 0;
+        this.lots(3, [3, 3, 3]);
         parkingLotObserver = new ParkingLotObserver();
     }
 
     park(vehicle) {
+        let lotNumber;
         if (vehicle == null) {
             throw new Error('unknown vehicle parked.');
         }
         if (this.isParkingLotFull()) {
             throw new Error('parking lot is full.');
         }
-        for (let i = 0; i < this.parkingLots.length; i++) {
-            if (this.parkingLots[i] == undefined) {
-                this.parkingLots.fill(vehicle, i, i + 1)
+        if (vehicle.valueOf() == "Handicap") {
+            lotNumber = this.findLotNumberToParkForHandicap();
+        } else {
+            lotNumber = this.findLotNumberToPark();
+        }
+        for (let i = 0; i < this.parkingLots[lotNumber].length; i++) {
+            if (this.parkingLots[lotNumber][i] == undefined) {
+                this.parkingLots[lotNumber][i] = vehicle
+                this.counter++
                 return true;
             }
         }
-        this.parkingLots.push(vehicle);
-        return true;
     }
 
     unPark(vehicle) {
@@ -32,18 +39,21 @@ class ParkingLotSystem {
             throw new Error('null or undefined car unParked.');
         }
         for (let i = 0; i < this.parkingLots.length; i++) {
-            if (this.parkingLots[i] == vehicle) {
-                this.parkingLots.splice(i, 1, undefined)
-                parkingLotObserver.subscribe();
-                parkingLotObserver.getNotifyToOwner();
-                return true;
+            for (let j = 0; j < this.parkingLots.length; j++) {
+                if (this.parkingLots[i][j] == vehicle) {
+                    this.parkingLots[i][j] = undefined
+                    parkingLotObserver.subscribe();
+                    parkingLotObserver.getNotifyToOwner();
+                    this.counter--
+                    return true;
+                }
             }
         }
         throw new Error('unknown vehicle unParked.');
     }
 
     isParkingLotFull() {
-        if (this.parkingLots.length == parkingLotMaxSize ) {
+        if (this.counter == parkingLotMaxSize) {
             parkingLotObserver.subscribe()
             parkingLotObserver.getNotify();
             return true;
@@ -53,20 +63,56 @@ class ParkingLotSystem {
 
     findEmptySlots() {
         for (let i = 0; i < this.parkingLots.length; i++) {
-            if (this.parkingLots[i] == undefined) {
-                return i;
+            for (let j = 0; j < this.parkingLots.length; j++) {
+                if (this.parkingLots[i][j] == undefined) {
+                    let emptySpace = {lot: i, slot: j}
+                    return emptySpace;
+                }
             }
         }
-        return false;
+        return false
     }
 
     findMyCar(vehicle) {
         for (let i = 0; i < this.parkingLots.length; i++) {
-            if (this.parkingLots[i] == vehicle) {
-                return i;
+            for (let j = 0; j < this.parkingLots.length; j++) {
+                if (this.parkingLots[i][j] == vehicle) {
+                    let emptySpace = {lot: i, slot: j}
+                    return emptySpace;
+                }
             }
         }
-        return false;
+        return false
+    }
+
+    lots(numOdLot, capacity) {
+        this.parkingLots = [];
+        for (let i = 0; i < numOdLot; i++) {
+            this.parkingLots[i] = new Array();
+            for (let j = 0; j < capacity[i]; j++) {
+                this.parkingLots[i][j] = undefined;
+            }
+        }
+    }
+
+    findLotNumberToPark() {
+        for (let j = 0; j < this.parkingLots.length; j++) {
+            for (let i = 0; i < this.parkingLots[j].length; i++) {
+                if (this.parkingLots[i][j] == undefined) {
+                    return [i];
+                }
+            }
+        }
+    }
+
+    findLotNumberToParkForHandicap() {
+        for (let i = 0; i < this.parkingLots.length; i++) {
+            for (let j = 0; j < i; j++) {
+                if (this.parkingLots[j][i] == undefined) {
+                    return j;
+                }
+            }
+        }
     }
 }
 
